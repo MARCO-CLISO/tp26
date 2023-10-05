cb721bfe222f9cd6f622cf895700fb97ee696f5f
MARCO-CLISO <136631803+MARCO-CLISO@users.noreply.github.com>
2023-10-05 15:40:45 -0300

    Update main.go

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ func main() {
 	// Crear un mapa para almacenar los apellidos de los jugadores y la descripcion.
 	diccionario := map[string]string{
 		"Messi": "Edad:35 \nNombre completo:leonel andres messi cuccittini \nApodo:pulga \nFecha de nacimiento:24/06/1987 \nEstatura:171 \nPeso:72 ",
-		"gauto": "Edad:18 \nNombre completo:juan gauto \nApodo:juanchi \nFecha de nacimiento:02/06/2004 \nEstatura:170 \nPeso:70 ",
-		//TODO agregar un jugador de la seleccion
+		"gauto": "Edad:18 \nNombre completo:juan gauto \nApodo:juanchi \nFecha de nacimiento:02/06/2004 \nEstatura:170.200 \nPeso:10 ",
+		"marco": "Edad:16 \nNombre completo:marcos el crac \nApodo:juanchi \nFecha de nacimiento:021/4/2007 \nEstatura 375 \nPeso:1 ",
 		"Lautaro": "Edad: 26\nNombre completo: Lautaro Javier\nApellido: Martinez\nApodo: toro\nFecha de nacimiento:  22 de agosto de 1997\nEstatura:  174 cm\nPeso: 72 kg",
 		"Messi":   "Edad: 34\nNombre completo: Lionel Andres Messi \nApellido: Cuccittini\nApodo: Leo\nFecha de nacimiento: 24 de junio de 1987\nEstatura: 170 cm\nPeso: 72 kg",
 	}
